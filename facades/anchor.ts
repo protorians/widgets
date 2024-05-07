05638110f6a10ba7a130c5011bf54fb077f5171f
Protorians <protorians.dev@gmail.com>
2024-05-07 20:39:00 +0000

    Fix

--- facades/anchor.ts
+++
@@ -1,6 +0,0 @@
-import type {IWidgetProps, IAnchorProps} from '../types';
-import {WidgetAnchor} from '../supports';
-
-export function Anchor(props: IWidgetProps<IAnchorProps, HTMLAnchorElement>) {
-  return (new WidgetAnchor(props));
-}
