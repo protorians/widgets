36ce328b6c1dba907116d997c4b559fa4cc6f6c9
Protorians <protorians.dev@gmail.com>
2024-05-17 18:44:00 +0000

    Add Static types

---
+++ types/static.ts
@@ -0,0 +1,6 @@
+import { IAttributes } from './attributes';
+import { IWidgetElements  } from './widget';
+import { WidgetNode } from '../supports';
+
+
+export type IStaticWidgetNode<P extends IAttributes , E extends IWidgetElements> = typeof WidgetNode<P , E>;
