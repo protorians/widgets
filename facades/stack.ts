5a52f9ec590f87377da112e8550dbc10c12e351f
Protorians <protorians.dev@gmail.com>
2024-05-17 18:43:48 +0000

    Fix Stacks Widget

--- facades/stack.ts
+++ facades/stack.ts
@@ -1,6 +1,20 @@
-import type {IAttributesScope, IStackAttributes} from '../types';
-import {WidgetStack} from '../supports';
+import type {
+  IAttributesScope ,
+  IColumnAttributes ,
+  IRowAttributes ,
+  ISectionAttributes,
+} from '../types';
+import { WidgetColumn , WidgetRow , WidgetSection } from '../supports';
 
-export function Stack(props: IAttributesScope<IStackAttributes, HTMLElement>) {
-  return (new WidgetStack(props));
+export function Section(props: IAttributesScope<ISectionAttributes, HTMLElement>) {
+  return (new WidgetSection(props));
 }
+
+export function Row(props: IAttributesScope<IRowAttributes, HTMLElement>) {
+  return (new WidgetRow(props));
+}
+
+export function Column(props: IAttributesScope<IColumnAttributes, HTMLElement>) {
+  return (new WidgetColumn(props));
+}
+
--- supports/stack.ts
+++ supports/stack.ts
@@ -1,17 +1,59 @@
 import type {
-  IStackAttributes,
-  IWidget,
+  IAttributes ,
+  IColumnAttributes ,
+  IRowAttributes ,
+  ISectionAttributes , IStyle ,
+  IWidget , IWidgetElements ,
 } from '../types';
-import {WidgetNode} from './widget';
+import { WidgetNode } from './widget';
 
-export class WidgetStack
+export class WidgetSection
 
-  extends WidgetNode<IStackAttributes, HTMLElement>
+  extends WidgetNode<ISectionAttributes , HTMLElement>
 
-  implements IWidget<IStackAttributes, HTMLElement> {
+  implements IWidget<ISectionAttributes , HTMLElement> {
 
-  get tag(): string {
+  get tag () : string {
     return 'section';
+  }
+
+}
+
+
+export class WidgetRow
+
+  extends WidgetNode<IRowAttributes , HTMLElement>
+
+  implements IWidget<IRowAttributes , HTMLElement> {
+
+  get tag () {
+    return 'div';
+  }
+
+  static style<P extends IAttributes , E extends IWidgetElements> () : Partial<IStyle<P , E>> | undefined {
+    return {
+      display: 'flex' ,
+      flexDirection: 'row' ,
+    };
+  }
+
+}
+
+export class WidgetColumn
+
+  extends WidgetNode<IColumnAttributes , HTMLElement>
+
+  implements IWidget<IColumnAttributes , HTMLElement> {
+
+  get tag () {
+    return 'div';
+  }
+
+  static style<P extends IAttributes , E extends IWidgetElements> () : Partial<IStyle<P , E>> | undefined {
+    return {
+      display: 'flex' ,
+      flexDirection: 'column' ,
+    };
   }
 
 }
