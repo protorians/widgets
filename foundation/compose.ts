5391fcbc70156856dd71f4cf9e34262852f75811
Protorians <protorians.dev@gmail.com>
2024-10-01 11:30:25 +0000

    Upgrade Compose Definition

--- foundation/compose.ts
+++ foundation/compose.ts
@@ -1,12 +1,21 @@
-import {IAttributes , IParameters , IWidgetElements , IComposeConstruct , IWidget , ICompose} from '../types';
+import {
+  IAttributes ,
+  IParameters ,
+  IWidgetElements ,
+  IComposeConstruct ,
+  IWidget ,
+  IComponentFunction,
+} from '../types';
 import {Component} from './component';
 
 
+
 export function Compose<Parameters extends IParameters , P extends IAttributes , E extends IWidgetElements> (
+  alias: string,
   construct : IComposeConstruct<Parameters , P , E> ,
   initials : Parameters ,
-): ICompose<Parameters, P, E> {
+): IComponentFunction<Parameters> {
 
-  return Component((props : Parameters): IWidget<P , E> => construct({...initials , ...props}));
+  return Component<Parameters>(alias, (props : Parameters): IWidget<P , E> => construct({...initials , ...props}));
 
 }
