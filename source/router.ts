eca322a854e85d27c6d4e048c54d702758737db3
Protorians <protorians.dev@gmail.com>
2025-02-18 13:36:37 +0000

    Fix ClientRouter errors fallback

--- source/router.ts
+++ source/router.ts
@@ -1,11 +1,11 @@
 import type {
-  IRouter, IRouterBaseRoute,
-  IRouterBaseScheme,
-  IRouterConfig,
-  IRouterRoute,
-  IRouterScheme,
-  IRouterSignalMap,
-  ISignalStack
+    IRouter, IRouterBaseRoute,
+    IRouterBaseScheme,
+    IRouterConfig,
+    IRouterRoute,
+    IRouterScheme,
+    IRouterSignalMap,
+    ISignalStack
 } from "./types";
 import {SignalHook} from "./hooks";
 import {WidgetException} from "./errors";
@@ -13,148 +13,156 @@ import {WidgetException} from "./errors";
 
 export class ClientRouter<Scheme extends IRouterBaseScheme> implements IRouter<Scheme> {
 
-  static current: IRouter<any> | undefined;
+    static current: IRouter<any> | undefined;
 
+    protected _route: (IRouterRoute<any, keyof any> & IRouterBaseRoute) | undefined;
 
-  protected _route: (IRouterRoute<any, keyof any> & IRouterBaseRoute) | undefined;
+    protected _routes: IRouterScheme<Scheme> = {} as IRouterScheme<Scheme>;
 
-  protected _routes: IRouterScheme<Scheme> = {} as IRouterScheme<Scheme>;
+    protected _error_status: boolean = false;
 
-  signal: ISignalStack<IRouterSignalMap<Scheme>>;
+    signal: ISignalStack<IRouterSignalMap<Scheme>>;
 
-  constructor(
-    public readonly config: IRouterConfig<Scheme>
-  ) {
-    this.signal = new SignalHook.Stack<IRouterSignalMap<Scheme>>;
-  }
-
-  get route(): (IRouterRoute<any, keyof any> & IRouterBaseRoute) | undefined {
-    return this._route;
-  }
-
-  get url(): URL {
-    return new URL(location.href);
-  }
-
-  get uri(): string {
-    return this.config.useHash ? this.url.hash.substring(1) : this.url.pathname;
-  }
-
-  //
-  // currentUri(){
-  //   return this.config.useHash ?
-  // }
-  //
-  get host(): string {
-    return this.url.host
-  }
-
-  get port(): string {
-    return this.url.port
-  }
-
-  get protocol(): string {
-    return this.url.protocol
-  }
-
-  get secured(): boolean {
-    return this.url.protocol == 'https';
-  }
-
-  get routes(): IRouterScheme<Scheme> {
-    return this._routes;
-  }
-
-  query<K extends keyof Scheme>(): Scheme[K] {
-    const accumulate: Scheme[K] = {} as Scheme[K]
-    const params = new URLSearchParams((new URL(this.url)).search);
-    params.forEach((value, key) => accumulate[key] = value)
-    return accumulate;
-  }
-
-  use<K extends keyof Scheme>(route: IRouterRoute<Scheme, K>): this {
-    this._routes[route.path] = {...route, ...this.parses(route.path.toString())};
-    return this;
-  }
-
-  open<K extends keyof Scheme>(to: K | string, props?: Scheme[K]): this {
-    props = props || {} as Scheme[K]
-    const url = `${window.location.origin}${
-      this.config.useHash === true
-        ? `${window.location.pathname}#`
-        : `${this.config.baseUrl || ''}`
-    }${to.toString()}`;
-
-    window.history.pushState(props || {}, url, `${url}`);
-    this.resolve(to.toString(), props)
-    return this;
-  }
-
-  resolve<K extends keyof Scheme>(uri: string, props?: Scheme[K]): IRouterRoute<Scheme, keyof Scheme> & IRouterBaseRoute {
-    // const oldRoute = this._route;
-
-    for (const route of Object.values(this.routes)) {
-      const match = this.match(uri, route.pattern);
-      if (match) {
-        const params = this.excavation(route.parameters, match);
-
-        this._route = route;
-        props = props || {} as Scheme[K];
-
-        this.signal.dispatch('navigate', {
-          route: route as IRouterRoute<Scheme, keyof Scheme> & IRouterBaseRoute,
-          params: params as Scheme[keyof Scheme],
-          props,
-        })
-
-        return route as IRouterRoute<Scheme, keyof Scheme> & IRouterBaseRoute;
-      }
+    constructor(
+        public readonly config: IRouterConfig<Scheme>
+    ) {
+        this.signal = new SignalHook.Stack<IRouterSignalMap<Scheme>>;
     }
-    throw (new WidgetException(`No route found for path: ${uri}`)).show();
-  }
 
-  parses(uri: string): IRouterBaseRoute {
-    const paramList: string[] = [];
-    const regexPath = uri.replace(/:(\w+)/g, (_: string, key: string) => {
-      paramList.push(key);
-      return '([^\\/]+)';
-    });
-    return {pattern: new RegExp(`^${regexPath}$`), parameters: paramList} as IRouterBaseRoute;
-  }
+    get route(): (IRouterRoute<any, keyof any> & IRouterBaseRoute) | undefined {
+        return this._route;
+    }
 
-  match(uri: string, regex: string): RegExpMatchArray | null {
-    return uri.match(regex);
-  }
+    get url(): URL {
+        return new URL(location.href);
+    }
 
-  excavation(paramList: string[], match: RegExpMatchArray): Scheme[keyof Scheme] {
-    return match.slice(1).reduce((params, value, index) => {
-      params[paramList[index]] = value;
-      return params;
-    }, {} as Scheme[keyof Scheme]);
-  }
+    get uri(): string {
+        return this.config.useHash ? this.url.hash.substring(1) : (
+            this.url.pathname == '/' ? '' : this.url.pathname
+        );
+    }
 
-  run(): this {
-    const uri = this.uri || this.config.index.toString();
-    (this.constructor as typeof ClientRouter).current = (this.constructor as typeof ClientRouter).current || this;
+    //
+    // currentUri(){
+    //   return this.config.useHash ?
+    // }
+    //
+    get host(): string {
+        return this.url.host
+    }
 
-    window.addEventListener('popstate', () =>
-      this.open(this.uri.trim().length > 0 ? this.uri : uri)
-    )
+    get port(): string {
+        return this.url.port
+    }
 
-    this.resolve(uri);
-    return this;
-  }
+    get protocol(): string {
+        return this.url.protocol
+    }
+
+    get secured(): boolean {
+        return this.url.protocol == 'https';
+    }
+
+    get routes(): IRouterScheme<Scheme> {
+        return this._routes;
+    }
+
+    query<K extends keyof Scheme>(): Scheme[K] {
+        const accumulate: Scheme[K] = {} as Scheme[K]
+        const params = new URLSearchParams((new URL(this.url)).search);
+        params.forEach((value, key) => accumulate[key] = value)
+        return accumulate;
+    }
+
+    use<K extends keyof Scheme>(route: IRouterRoute<Scheme, K>): this {
+        this._routes[route.path] = {...route, ...this.parses(route.path.toString())};
+        return this;
+    }
+
+    open<K extends keyof Scheme>(to: K | string, props?: Scheme[K]): this {
+        props = props || {} as Scheme[K]
+        const url = `${window.location.origin}${
+            this.config.useHash === true
+                ? `${window.location.pathname}#`
+                : `${this.config.baseUrl || ''}`
+        }${to.toString()}`;
+
+        window.history.pushState(props || {}, url, `${url}`);
+        this.resolve(to.toString(), props)
+        return this;
+    }
+
+    resolve<K extends keyof Scheme>(uri: string, props?: Scheme[K]): IRouterRoute<Scheme, keyof Scheme> & IRouterBaseRoute {
+
+        for (const route of Object.values(this.routes)) {
+            const match = this.match(uri, route.pattern);
+            if (match) {
+                this._error_status = false;
+
+                const params = this.excavation(route.parameters, match);
+
+                this._route = route;
+                props = props || {} as Scheme[K];
+
+                this.signal.dispatch('navigate', {
+                    route: route as IRouterRoute<Scheme, keyof Scheme> & IRouterBaseRoute,
+                    params: params as Scheme[keyof Scheme],
+                    props,
+                })
+
+                return route as IRouterRoute<Scheme, keyof Scheme> & IRouterBaseRoute;
+            }
+        }
+
+        if (this.config.errors && ("404" in this.config.errors) && !this._error_status) {
+            this._error_status = true;
+            return this.resolve(this.config.errors["404"] as string, props)
+        }
+
+        throw (new WidgetException(`No route found for path: ${uri}`)).show();
+    }
+
+    parses(uri: string): IRouterBaseRoute {
+        const paramList: string[] = [];
+        const regexPath = uri.replace(/:(\w+)/g, (_: string, key: string) => {
+            paramList.push(key);
+            return '([^\\/]+)';
+        });
+        return {pattern: new RegExp(`^${regexPath}$`), parameters: paramList} as IRouterBaseRoute;
+    }
+
+    match(uri: string, regex: string): RegExpMatchArray | null {
+        return uri.match(regex);
+    }
+
+    excavation(paramList: string[], match: RegExpMatchArray): Scheme[keyof Scheme] {
+        return match.slice(1).reduce((params, value, index) => {
+            params[paramList[index]] = value;
+            return params;
+        }, {} as Scheme[keyof Scheme]);
+    }
+
+    run(): this {
+        const uri = this.uri || this.config.index.toString();
+        (this.constructor as typeof ClientRouter).current = (this.constructor as typeof ClientRouter).current || this;
+
+        window.addEventListener('popstate', () => this.open(this.uri.trim().length > 0 ? this.uri : uri))
+
+        this.resolve(uri);
+        return this;
+    }
 
 }
 
 export function useRouter<Scheme extends IRouterBaseScheme>(config: IRouterConfig<Scheme>) {
-  return (new ClientRouter<Scheme>(config))
+    return (new ClientRouter<Scheme>(config))
 }
 
 export function router<Scheme extends IRouterBaseScheme>(): IRouter<Scheme> | undefined {
-  return (ClientRouter.current || undefined) as (IRouter<Scheme> | undefined)
+    return (ClientRouter.current || undefined) as (IRouter<Scheme> | undefined)
 }
 
 export function route<Scheme extends IRouterBaseScheme>(): (IRouterRoute<Scheme, keyof Scheme> & IRouterBaseRoute) | undefined {
-  return (router<Scheme>()?.route || undefined) as ((IRouterRoute<Scheme, keyof Scheme> & IRouterBaseRoute) | undefined)
+    return (router<Scheme>()?.route || undefined) as ((IRouterRoute<Scheme, keyof Scheme> & IRouterBaseRoute) | undefined)
 }
--- source/types/router.ts
+++ source/types/router.ts
@@ -3,79 +3,151 @@ import {ViewWidget} from "../view";
 import type {ISignalStack} from "./signals";
 
 
+/**
+ * Errors path configuration
+ */
+export type IRouterConfigErrors<Scheme extends IRouterBaseScheme> = {
+
+    /**
+     * Bad Request
+     */
+    400?: keyof Scheme;
+
+    /**
+     * 401 Unauthorized
+     */
+    401?: keyof Scheme;
+
+    /**
+     * 403 Forbidden
+     */
+    403?: keyof Scheme;
+
+    /**
+     * 404 Not Found
+     */
+    404?: keyof Scheme;
+
+    /**
+     * 405 Method Not Allowed
+     */
+    405?: keyof Scheme;
+
+    /**
+     * 500 Internal Server Error
+     */
+    500?: keyof Scheme;
+
+    /**
+     * 502 Bad Gateway
+     */
+    502?: keyof Scheme;
+
+    /**
+     * 503 Service Unavailable
+     */
+    503?: keyof Scheme;
+
+    /**
+     * 504 Gateway Timeout
+     */
+    504?: keyof Scheme;
+};
+
+/**
+ * Router Configuration
+ */
 export type IRouterConfig<Scheme extends IRouterBaseScheme> = {
-  useHash?: boolean;
-  baseUrl?: string;
-  index: keyof Scheme;
+
+    /**
+     * Use hashtag or not in application URL
+     */
+    useHash?: boolean;
+
+    /**
+     * Base URL
+     */
+    baseUrl?: string;
+
+    /**
+     * Index view fallback
+     */
+    index: keyof Scheme;
+
+    /**
+     * Error views
+     */
+    errors?: IRouterConfigErrors<Scheme>
 }
+
 
 export type IRouteView<T extends Object> = IViewConstructor<T>
 
 export type IRouterRoute<Scheme extends IRouterBaseScheme, K extends keyof Scheme> = {
-  path: K;
-  view: typeof ViewWidget<Scheme[K]>;
-  options?: IViewOptions<Scheme[K]>
+    path: K;
+    view: typeof ViewWidget<Scheme[K]>;
+    options?: IViewOptions<Scheme[K]>
 }
 
 export type IRouterBaseRoute = {
-  // method?: HttpMethod;
-  parameters: string[];
-  pattern: RegExp;
+    // method?: HttpMethod;
+    parameters: string[];
+    pattern: RegExp;
 }
 
 export interface IRouterBaseScheme {
-  [K: string]: Object;
+    [K: string]: Object;
 }
 
 export type IRouterScheme<Scheme extends IRouterBaseScheme> = {
-  [K in keyof Scheme]: IRouterRoute<Scheme, K> & IRouterBaseRoute
+    [K in keyof Scheme]: IRouterRoute<Scheme, K> & IRouterBaseRoute
 }
 
 export interface IRouter<Scheme extends IRouterBaseScheme> {
-  readonly config: IRouterConfig<Scheme>;
+    readonly config: IRouterConfig<Scheme>;
 
-  signal: ISignalStack<IRouterSignalMap<Scheme>>;
+    signal: ISignalStack<IRouterSignalMap<Scheme>>;
 
-  get routes(): IRouterScheme<Scheme>;
+    get routes(): IRouterScheme<Scheme>;
 
-  get route(): (IRouterRoute<any, keyof any> & IRouterBaseRoute) | undefined;
+    get route(): (IRouterRoute<any, keyof any> & IRouterBaseRoute) | undefined;
 
-  get url(): URL;
+    get url(): URL;
 
-  get uri(): string;
+    get uri(): string;
 
-  get host(): string;
+    get host(): string;
 
-  get port(): string;
+    get port(): string;
 
-  get protocol(): string;
+    get protocol(): string;
 
-  get secured(): boolean;
+    get secured(): boolean;
 
-  query<K extends keyof Scheme>(): Scheme[K];
+    query<K extends keyof Scheme>(): Scheme[K];
 
-  use<K extends keyof Scheme>(route: IRouterRoute<Scheme, K>): this;
+    use<K extends keyof Scheme>(route: IRouterRoute<Scheme, K>): this;
 
-  open<K extends keyof Scheme>(to: string, props: Scheme[K]): this;
+    open<K extends keyof Scheme>(to: string, props: Scheme[K]): this;
 
-  resolve<K extends keyof Scheme>(uri: string, props: Scheme[K]): IRouterRoute<Scheme, keyof Scheme> & IRouterBaseRoute;
+    resolve<K extends keyof Scheme>(uri: string, props: Scheme[K]): IRouterRoute<Scheme, keyof Scheme> & IRouterBaseRoute;
 
-  parses(uri: string): IRouterBaseRoute;
+    parses(uri: string): IRouterBaseRoute;
 
-  match(uri: string, regex: string): RegExpMatchArray | null;
+    match(uri: string, regex: string): RegExpMatchArray | null;
 
-  excavation(paramList: string[], match: RegExpMatchArray): Scheme[keyof Scheme];
+    excavation(paramList: string[], match: RegExpMatchArray): Scheme[keyof Scheme];
 
-  run(): this;
+    run(): this;
 }
 
 
 export type IRouteNavigateSignal<Scheme extends IRouterBaseScheme> = {
-  params: Scheme[keyof Scheme];
-  route: IRouterRoute<Scheme, keyof Scheme> & IRouterBaseRoute;
-  props: Scheme[keyof Scheme];
+    params: Scheme[keyof Scheme];
+    route: IRouterRoute<Scheme, keyof Scheme> & IRouterBaseRoute;
+    props: Scheme[keyof Scheme];
 }
 
 export type IRouterSignalMap<Scheme extends IRouterBaseScheme> = {
-  navigate: IRouteNavigateSignal<Scheme>;
+    navigate: IRouteNavigateSignal<Scheme>;
 }
