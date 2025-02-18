edfa54ca3dbf22420a75a3c0313fc90be0fb39e0
Protorians <protorians.dev@gmail.com>
2025-02-18 13:39:44 +0000

    Fix container composite type

--- source/types/container.ts
+++ source/types/container.ts
@@ -2,11 +2,11 @@ import type {Sizer} from "../enums";
 import type {IChildrenSupported, IWidgetDeclaration} from "./widget";
 import type {IChildren} from "./children";
 import type {ICommonAttributes} from "./attributes";
-import {IStyle} from "./style";
+import type {IStyleSheetDeclarations} from "./style";
 
 export interface IContainerProps {
-  size?: number | string | Sizer;
-  style?: IStyle;
-  contentAttributes?: Omit<IWidgetDeclaration<HTMLElement, ICommonAttributes>, 'children'>;
-  children: IChildren<IChildrenSupported>;
+    size?: number | string | Sizer;
+    style?: IStyleSheetDeclarations;
+    contentAttributes?: Omit<IWidgetDeclaration<HTMLElement, ICommonAttributes>, 'children'>;
+    children: IChildren<IChildrenSupported>;
 }
