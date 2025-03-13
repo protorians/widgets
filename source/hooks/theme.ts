0384e21d33678d5a4cc4198a77b371e7524af73c
Protorians <protorians.dev@gmail.com>
2025-03-13 22:10:20 +0000

    Extract to new `@protorians/widget-ui`

--- source/hooks/theme.ts
+++
@@ -1,9 +0,0 @@
-import {ITheme} from "../types/index.js";
-import {Environment} from "@protorians/core";
-
-export function useTheme(settings: ITheme) {
-    if (Environment.Client) {
-        Object.entries(settings)
-            .forEach(([key, value]) => document.documentElement.style.setProperty(`--widget-theme-${key}`, `${value}`))
-    }
-}
