bcb09934140bc2a048088e7bcb9f46334c6b4da4
Protorians <protorians.dev@gmail.com>
2025-03-16 18:43:32 +0000

    Fix createRef helper

--- source/ref.ts
+++ source/ref.ts
@@ -4,26 +4,26 @@ import {WidgetException} from "./errors/index.js";
 
 export class RefWidget<E extends HTMLElement, A extends IAttributes> implements IRef<E, A> {
 
-  protected widget: IWidgetNode<E, A> | undefined;
+    protected widget: IWidgetNode<E, A> | undefined;
 
-  get current(): IWidgetNode<E, A> | undefined {
-    return this.widget;
-  }
+    get current(): IWidgetNode<E, A> | undefined {
+        return this.widget;
+    }
 
-  attach(widget: IWidgetNode<E, A>): this {
-    if (typeof this.widget !== 'undefined')
-      throw (new WidgetException('Thee reference is already defined')).show()
-    this.widget = widget;
-    return this;
-  }
+    attach(widget: IWidgetNode<E, A>): this {
+        if (typeof this.widget !== 'undefined')
+            throw (new WidgetException('Thee reference is already defined')).show()
+        this.widget = widget;
+        return this;
+    }
 
-  detach(): void {
-    if (typeof this.widget === 'undefined')
-      throw (new WidgetException('The reference is not defined')).show()
-    this.widget = undefined;
-  }
+    detach(): void {
+        if (typeof this.widget === 'undefined')
+            throw (new WidgetException('The reference is not defined')).show()
+        this.widget = undefined;
+    }
 }
 
-export function createRef() {
-  return new RefWidget;
+export function createRef<E extends HTMLElement, A extends IAttributes>() {
+    return new RefWidget<E, A>();
 }
