1b0e69ab2303a2254cddfafbbce4112c9660a690
Protorians <protorians.dev@gmail.com>
2025-03-16 17:05:01 +0000

    Add `AsideFrameWidget`, `HeaderFrameWidget`, `FooterFrameWidget` and `MainFrameWidget`

--- source/overlay/layer.ts
+++ source/overlay/layer.ts
@@ -9,9 +9,57 @@ import {Composable, Mountable} from "../decorators.js";
 @Mountable()
 @Composable()
 export class SectionWidget extends WidgetNode<HTMLElement, ICommonAttributes> {
-  get tag(): string {
-    return 'section'
-  };
+    get tag(): string {
+        return 'section'
+    };
+}
+
+
+/**
+ * @description AsideFrame Widget
+ */
+@Mountable()
+@Composable()
+export class AsideFrameWidget extends WidgetNode<HTMLElement, ICommonAttributes> {
+    get tag(): string {
+        return 'aside'
+    };
+}
+
+
+/**
+ * @description HeaderFrame Widget
+ */
+@Mountable()
+@Composable()
+export class HeaderFrameWidget extends WidgetNode<HTMLElement, ICommonAttributes> {
+    get tag(): string {
+        return 'header'
+    };
+}
+
+
+/**
+ * @description FooterFrame Widget
+ */
+@Mountable()
+@Composable()
+export class FooterFrameWidget extends WidgetNode<HTMLElement, ICommonAttributes> {
+    get tag(): string {
+        return 'footer'
+    };
+}
+
+
+/**
+ * @description MainFrame Widget
+ */
+@Mountable()
+@Composable()
+export class MainFrameWidget extends WidgetNode<HTMLElement, ICommonAttributes> {
+    get tag(): string {
+        return 'main'
+    };
 }
 
 /**
@@ -20,7 +68,7 @@ export class SectionWidget extends WidgetNode<HTMLElement, ICommonAttributes> {
  * @constructor
  */
 export function Layer(declaration: IWidgetDeclaration<HTMLElement, ICommonAttributes>): WidgetNode<HTMLElement, ICommonAttributes> {
-  return new WidgetNode(declaration)
+    return new WidgetNode(declaration)
 }
 
 /**
@@ -29,5 +77,41 @@ export function Layer(declaration: IWidgetDeclaration<HTMLElement, ICommonAttrib
  * @constructor
  */
 export function Section(declaration: IWidgetDeclaration<HTMLElement, ICommonAttributes>): SectionWidget {
-  return new SectionWidget(declaration)
+    return new SectionWidget(declaration)
+}
+
+/**
+ * @description Construct's Function of `MainFrameWidget`
+ * @param declaration
+ * @constructor
+ */
+export function MainFrame(declaration: IWidgetDeclaration<HTMLElement, ICommonAttributes>): MainFrameWidget {
+    return new MainFrameWidget(declaration)
+}
+
+/**
+ * @description Construct's Function of `HeaderFrameWidget`
+ * @param declaration
+ * @constructor
+ */
+export function HeaderFrame(declaration: IWidgetDeclaration<HTMLElement, ICommonAttributes>): HeaderFrameWidget {
+    return new HeaderFrameWidget(declaration)
+}
+
+/**
+ * @description Construct's Function of `FooterFrameWidget`
+ * @param declaration
+ * @constructor
+ */
+export function FooterFrame(declaration: IWidgetDeclaration<HTMLElement, ICommonAttributes>): FooterFrameWidget {
+    return new FooterFrameWidget(declaration)
+}
+
+/**
+ * @description Construct's Function of `AsideFrameWidget`
+ * @param declaration
+ * @constructor
+ */
+export function AsideFrame(declaration: IWidgetDeclaration<HTMLElement, ICommonAttributes>): AsideFrameWidget {
+    return new AsideFrameWidget(declaration)
 }
