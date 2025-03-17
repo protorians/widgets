589821edc2d3bfb12d2bf2fab1adf19afffbc233
Protorians <protorians.dev@gmail.com>
2025-03-17 16:45:51 +0000

    Add Widget global configuration

---
+++ source/main.ts
@@ -0,0 +1,8 @@
+import {StyleWidget} from "./style.js";
+
+
+StyleWidget.alias
+    .set('paddingX', ['paddingLeft', 'paddingRight'])
+    .set('paddingY', ['paddingTop', 'paddingBottom'])
+    .set('marginX', ['marginLeft', 'marginRight'])
+    .set('marginY', ['marginTop', 'marginBottom']);
