01135eaad8e238476b02e5c26356ac0acc832577
Protorians <protorians.dev@gmail.com>
2025-03-17 16:46:10 +0000

    Add `WidgetElevation`

--- source/enums.ts
+++ source/enums.ts
@@ -1,141 +1,151 @@
 export enum HttpMethod {
-  POST = 'POST',
-  GET = 'GET',
-  PUT = 'PUT',
-  PATCH = 'PATCH',
-  DELETE = 'DELETE',
-  OPTION = 'OPTION',
+    POST = 'POST',
+    GET = 'GET',
+    PUT = 'PUT',
+    PATCH = 'PATCH',
+    DELETE = 'DELETE',
+    OPTION = 'OPTION',
 }
 
 export enum ColorSchemeType {
-  Light = 'light',
-  Dark = 'dark',
+    Light = 'light',
+    Dark = 'dark',
 }
 
 export enum ColorimetricType {
-  Hex = 'hex',
-  Oklch = 'oklch',
+    Hex = 'hex',
+    Oklch = 'oklch',
 }
 
 export enum InsertionPosition {
-  BeforeBegin = 'BeforeBegin',
-  BeforeEnd = 'BeforeEnd',
-  AfterBegin = 'AfterBegin',
-  AfterEnd = 'AfterEnd',
+    BeforeBegin = 'BeforeBegin',
+    BeforeEnd = 'BeforeEnd',
+    AfterBegin = 'AfterBegin',
+    AfterEnd = 'AfterEnd',
 }
 
 export enum InsertionSiblingPosition {
-  Before = 'before',
-  After = 'After',
+    Before = 'before',
+    After = 'After',
 }
 
 export enum EdgePosition {
-  Start = 'start',
-  Center = 'center',
-  End = 'end',
+    Start = 'start',
+    Center = 'center',
+    End = 'end',
 }
 
 export enum AligningDirection {
-  Row = 'row',
-  RowReverse = 'row-reverse',
-  Column = 'column',
-  ColumnReverse = 'column-reverse',
+    Row = 'row',
+    RowReverse = 'row-reverse',
+    Column = 'column',
+    ColumnReverse = 'column-reverse',
 }
 
 export enum AligningProperty {
-  JustifyContent = "justify-content",
-  AlignItems = "align-items",
-  AlignSelf = "align-self",
-  AlignContent = "align-content",
+    JustifyContent = "justify-content",
+    AlignItems = "align-items",
+    AlignSelf = "align-self",
+    AlignContent = "align-content",
 }
 
 export enum Aligning {
-  Start = 'start',
-  Center = 'center',
-  End = 'end',
-  Left = 'left',
-  Right = 'right',
-  FlexStart = 'flex-start',
-  FlexEnd = 'flex-end',
-  SpaceBetween = 'space-between',
-  SpaceAround = 'space-around',
-  SpaceEvenly = 'space-evenly',
-  Normal = 'normal',
-  Stretch = 'stretch',
-  SafeCenter = 'safe center',
-  UnsafeCenter = 'unsafe center',
-  First = 'first',
-  FirstBaseline = 'first baseline',
-  Last = 'last',
-  LastBaseline = 'last baseline',
-  Inherit = 'inherit',
-  Initial = 'initial',
-  Unset = 'unset',
+    Start = 'start',
+    Center = 'center',
+    End = 'end',
+    Left = 'left',
+    Right = 'right',
+    FlexStart = 'flex-start',
+    FlexEnd = 'flex-end',
+    SpaceBetween = 'space-between',
+    SpaceAround = 'space-around',
+    SpaceEvenly = 'space-evenly',
+    Normal = 'normal',
+    Stretch = 'stretch',
+    SafeCenter = 'safe center',
+    UnsafeCenter = 'unsafe center',
+    First = 'first',
+    FirstBaseline = 'first baseline',
+    Last = 'last',
+    LastBaseline = 'last baseline',
+    Inherit = 'inherit',
+    Initial = 'initial',
+    Unset = 'unset',
 }
 
 export enum TransitionMode {
-  Entry = 'entry',
-  Out = 'out',
+    Entry = 'entry',
+    Out = 'out',
 }
 
 export enum ToggleOption {
-  Visibility = 'visibility',
-  Activity = 'activity',
-  Interactivity = 'interactivity',
-  Stase = 'stase',
+    Visibility = 'visibility',
+    Activity = 'activity',
+    Interactivity = 'interactivity',
+    Stase = 'stase',
 }
 
 export enum TreatmentQueueStatus {
-  Continue = 'continue',
-  Cancel = 'cancel',
-  Exit = 'exit',
-}
-
-export enum WidgetsNativeProperty {
-  Signal = 'signal',
-  Ref = 'ref',
-  Children = 'children',
-  Style = 'style',
-  ClassName = 'className',
-  Data = 'data',
-  Nsa = 'nsa',
-  On = 'on',
-  Listen = 'listen',
-  Features = 'features',
+    Continue = 'continue',
+    Cancel = 'cancel',
+    Exit = 'exit',
 }
 
 export enum TextAligning {
-  Left = 'left',
-  Center = 'center',
-  Right = 'right',
-  Justify = 'justify',
+    Left = 'left',
+    Center = 'center',
+    Right = 'right',
+    Justify = 'justify',
 }
 
 export enum Sizer {
-  XS = 'x-small',
-  S = 'small',
-  M = 'medium',
-  L = 'large',
-  XL = 'x-large',
+    XS = 'x-small',
+    S = 'small',
+    M = 'medium',
+    L = 'large',
+    XL = 'x-large',
 }
 
 export enum AbsoluteUnit {
-  Cm = 'cm',
-  Mm = 'mm',
-  In = 'in',
-  Px = 'px',
-  Pt = 'pt',
-  Pc = 'pc',
+    Cm = 'cm',
+    Mm = 'mm',
+    In = 'in',
+    Px = 'px',
+    Pt = 'pt',
+    Pc = 'pc',
 }
 
 export enum RelativeUnit {
-  Em = 'em',
-  Ex = 'ex',
-  Ch = 'ch',
-  Rem = 'rem',
-  Vw = 'vw',
-  Vh = 'vh',
-  VMin = 'vmin',
-  VMax = 'vmax',
-  Percent = '%',
+    Em = 'em',
+    Ex = 'ex',
+    Ch = 'ch',
+    Rem = 'rem',
+    Vw = 'vw',
+    Vh = 'vh',
+    VMin = 'vmin',
+    VMax = 'vmax',
+    Percent = '%',
+}
+
+export enum WidgetsNativeProperty {
+    Signal = 'signal',
+    Ref = 'ref',
+    Children = 'children',
+    Style = 'style',
+    ClassName = 'className',
+    Data = 'data',
+    Nsa = 'nsa',
+    On = 'on',
+    Listen = 'listen',
+    Features = 'features',
+    Elevate = 'elevate',
+}
+
+export enum WidgetElevation {
+    None = 0,
+    Base = 9,
+    Standard = 49,
+    Float = 99,
+    Overlay = 999,
+    Critical = 9999,
 }
