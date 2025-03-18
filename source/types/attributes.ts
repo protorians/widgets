83cd5323499a24e1369ad97d1101c5492f35b2b2
Protorians <protorians.dev@gmail.com>
2025-03-18 13:12:08 +0000

    Add Accessibility `aria` attributes types

--- source/types/attributes.ts
+++ source/types/attributes.ts
@@ -1,68 +1,136 @@
+import {PopupType} from "../enums.js";
 import type {
-  IBoolean,
-  IFormRelationship,
-  ILoading,
-  IOnOff,
-  IReferrerPolicy,
-  ISandbox,
-  ITarget,
-  IYesNo
+    IBoolean,
+    IFormRelationship,
+    ILoading,
+    IOnOff,
+    IReferrerPolicy,
+    ISandbox,
+    ISorting,
+    ITarget,
+    IUndefined,
+    IYesNo
 } from "./value.js";
 import {ICallable} from "./widget.js";
 
 export type IAttributes = Partial<
-  ICommonAttributes
-  | IVideoAttributes
-  | ISourceProps
-  | IPictureAttributes
-  | IAudioAttributes
-  | IIFrameAttributes
-  | IArticleAttributes
-  | IHeadingAttributes
-  | IParagraphAttributes
-  | IStrongAttributes
-  | IHelmetAttributes
-  | IFrameAttributes
-  | IFooterAttributes
-  | ILinkAttributes
-  | ISpanAttributes
-  | ISectionAttributes
-  | ITableAttributes
-  | ITableCellAttributes
-  | ITableRowAttributes
-  | ITableCaptionAttributes
-  | ITableFooterAttributes
-  | ITableHeadAttributes
-  | ITableBodyAttributes
-  | IButtonAttributes
-  | IInputAttributes
-  | IOptionGroupAttributes
-  | IOptionAttributes
-  | ISelectAttributes
-  | ITextareaAttributes
-  | ILabelAttributes
-  | IInputableAttributes
-  | IFormAttributes
-  | IProgressAttributes
+    ICommonAttributes
+    | IVideoAttributes
+    | ISourceProps
+    | IPictureAttributes
+    | IAudioAttributes
+    | IIFrameAttributes
+    | IArticleAttributes
+    | IHeadingAttributes
+    | IParagraphAttributes
+    | IStrongAttributes
+    | IHelmetAttributes
+    | IFrameAttributes
+    | IFooterAttributes
+    | ILinkAttributes
+    | ISpanAttributes
+    | ISectionAttributes
+    | ITableAttributes
+    | ITableCellAttributes
+    | ITableRowAttributes
+    | ITableCaptionAttributes
+    | ITableFooterAttributes
+    | ITableHeadAttributes
+    | ITableBodyAttributes
+    | IButtonAttributes
+    | IInputAttributes
+    | IOptionGroupAttributes
+    | IOptionAttributes
+    | ISelectAttributes
+    | ITextareaAttributes
+    | ILabelAttributes
+    | IInputableAttributes
+    | IFormAttributes
+    | IProgressAttributes
 >
 
 export type IOperatingProps<T> = {
-  [K in keyof T]: T[keyof T]
+    [K in keyof T]: T[keyof T]
 }
 
-export interface ICommonAttributes {
-  accesskey?: string;
-  contenteditable?: string;
-  dir?: string;
-  id?: string;
-  lang?: string;
-  title?: string;
-  tabindex?: number;
-  spellcheck?: boolean;
-  draggable?: boolean;
-  hidden?: boolean;
-  translate?: IYesNo;
-  rel?: string;
+export interface IAccessibilityAttributes {
+    ariaAutocomplete?: "none" | "inline" | "list" | "both";
+    ariaChecked?: IBoolean | "mixed";
+    ariaDisabled?: IBoolean;
+    ariaErrormessage?: string;
+    ariaExpanded?: IBoolean | IUndefined;
+    ariaHaspopup?: IBoolean | PopupType;
+    ariaModal?: IBoolean;
+    ariaHidden?: IBoolean | IUndefined;
+    ariaInvalid?: "grammar" | "spelling" | IBoolean;
+    ariaLabel?: string;
+    ariaLevel?: number;
+    ariaMultiline?: IBoolean;
+    ariaMultiselectable?: string;
+    ariaOrientation?: string;
+    ariaPlaceholder?: string;
+    ariaPressed?: IBoolean;
+    ariaReadonly?: IBoolean;
+    ariaRequired?: IBoolean;
+    ariaSelected?: IBoolean | IUndefined;
+    ariaSort?: ISorting;
+    ariaValuemax?: number;
+    ariaValuemin?: number;
+    ariaValuenow?: number;
+    ariaValuetext?: string;
+    ariaCurrent?: IBoolean | "page" | "step" | "location" | "date" | "time";
+    ariaKeyshortcuts?: string;
+    ariaRoledescription?: string;
+    ariaBraillelabel?: string;
+    ariaBrailleroledescription?: string;
+    ariaColindextext?: string;
+    ariaRowindextext?: string;
+}
+
+export interface IAccessibilityRegionAttributes {
+    ariaBusy?: IBoolean;
+    ariaLive?: "assertive" | "off" | "polite";
+    ariaRelevant?: "additions" | "all" | "removals" | "text";
+    ariaAtomic?: IBoolean;
+}
+
+export interface IAccessibilityDragAndDropAttributes {
+    ariaDropeffect?: "copy" | "execute" | "link" | "move" | "none" | "popup";
+    ariaGrabbed?: IBoolean | IUndefined;
+}
+
+export interface IAccessibilityRelationshipAttributes {
+    ariaActivedescendant?: string;
+    ariaColcount?: number;
+    ariaColindex?: number;
+    ariaControls?: string;
+    ariaDescribedby?: string;
+    ariaDescription?: string;
+    ariaDetails?: string;
+    ariaFlowto?: string;
+    ariaLabelledby?: string;
+    ariaOwns?: string;
+    ariaPosinset?: number;
+    ariaRowcount?: number;
+    ariaRowindex?: number;
+    ariaRowspan?: number;
+    ariaSetsize?: number;
+}
+
+export interface ICommonAttributes extends IAccessibilityAttributes, IAccessibilityRegionAttributes, IAccessibilityDragAndDropAttributes, IAccessibilityRelationshipAttributes {
+    role?: string;
+    accesskey?: string;
+    contenteditable?: string;
+    dir?: string;
+    id?: string;
+    lang?: string;
+    title?: string;
+    tabindex?: number;
+    spellcheck?: boolean;
+    draggable?: boolean;
+    hidden?: boolean;
+    translate?: IYesNo;
+    rel?: string;
 }
 
 export interface ISpanAttributes extends ICommonAttributes {
@@ -105,94 +173,94 @@ export interface IColumnAttributes extends ICommonAttributes {
 }
 
 export interface IGridAttributes extends ICommonAttributes {
-  grid?: string;
-  gridTemplateRows?: string;
-  gridTemplateColumns?: string;
-  gridTemplateAreas?: string;
-  gridAutoRows?: string;
-  gridAutoColumns?: string;
-  gridAutoFlow?: string;
-  gridRowGap?: string;
-  gridColumnGap?: string;
+    grid?: string;
+    gridTemplateRows?: string;
+    gridTemplateColumns?: string;
+    gridTemplateAreas?: string;
+    gridAutoRows?: string;
+    gridAutoColumns?: string;
+    gridAutoFlow?: string;
+    gridRowGap?: string;
+    gridColumnGap?: string;
 }
 
 export interface ILinkAttributes extends ICommonAttributes {
-  href: string;
-  hreflang?: string;
-  ping?: string;
-  referrerPolicy?: IReferrerPolicy;
-  target?: ITarget;
-  type?: string;
-  download?: string;
-  media?: string;
+    href: string;
+    hreflang?: string;
+    ping?: string;
+    referrerPolicy?: IReferrerPolicy;
+    target?: ITarget;
+    type?: string;
+    download?: string;
+    media?: string;
 }
 
 
 export type IInputType = 'text'
-  | 'button'
-  | 'color'
-  | 'date'
-  | 'datetime-local'
-  | 'email'
-  | 'file'
-  | 'hidden'
-  | 'image'
-  | 'month'
-  | 'number'
-  | 'password'
-  | 'radio'
-  | 'range'
-  | 'reset'
-  | 'search'
-  | 'submit'
-  | 'tel'
-  | 'time'
-  | 'url'
-  | 'week'
-  | 'checkbox';
+    | 'button'
+    | 'color'
+    | 'date'
+    | 'datetime-local'
+    | 'email'
+    | 'file'
+    | 'hidden'
+    | 'image'
+    | 'month'
+    | 'number'
+    | 'password'
+    | 'radio'
+    | 'range'
+    | 'reset'
+    | 'search'
+    | 'submit'
+    | 'tel'
+    | 'time'
+    | 'url'
+    | 'week'
+    | 'checkbox';
 
 
 export type IInputableCommonAttributes = {
-  form?: string;
-  formAction?: string;
-  formEnctype?: string;
-  formMethod?: 'get' | 'post';
-  formNovalidate?: boolean;
-  formTarget?: ITarget;
+    form?: string;
+    formAction?: string;
+    formEnctype?: string;
+    formMethod?: 'get' | 'post';
+    formNovalidate?: boolean;
+    formTarget?: ITarget;
 }
 
 
 export type IButtonType = 'button' | 'reset' | 'submit' | 'menu'
 
 export interface IButtonAttributes extends IInputableAttributes {
-  popoverTarget?: string;
-  popoverTargetAction?: string;
-  type?: IButtonType;
+    popoverTarget?: string;
+    popoverTargetAction?: string;
+    type?: IButtonType;
 }
 
 export interface IButtonAttributesBase extends IInputableAttributes {
-  onPress: ICallable<HTMLButtonElement, IButtonAttributes, MouseEvent | Event | UIEvent>;
-  onPressStart?: ICallable<HTMLButtonElement, IButtonAttributes, MouseEvent | Event | UIEvent>;
-  onPressEnd?: ICallable<HTMLButtonElement, IButtonAttributes, MouseEvent | Event | UIEvent>;
+    onPress: ICallable<HTMLButtonElement, IButtonAttributes, MouseEvent | Event | UIEvent>;
+    onPressStart?: ICallable<HTMLButtonElement, IButtonAttributes, MouseEvent | Event | UIEvent>;
+    onPressEnd?: ICallable<HTMLButtonElement, IButtonAttributes, MouseEvent | Event | UIEvent>;
 }
 
 export interface IInputAttributes extends IInputableAttributes {
-  type?: IInputType;
-  children?: undefined;
+    type?: IInputType;
+    children?: undefined;
 }
 
 
 export interface IOptionGroupAttributes extends ICommonAttributes {
-  disabled?: boolean;
-  label: string;
+    disabled?: boolean;
+    label: string;
 }
 
 
 export interface IOptionAttributes extends ICommonAttributes {
-  disabled?: boolean;
-  selected?: boolean;
-  label: string;
-  value?: string | number;
+    disabled?: boolean;
+    selected?: boolean;
+    label: string;
+    value?: string | number;
 }
 
 export interface ISelectAttributes extends IInputableAttributes {
@@ -202,102 +270,102 @@ export interface ITextareaAttributes extends IInputableAttributes {
 }
 
 export interface ILabelAttributes extends IInputableAttributes {
-  for?: string;
+    for?: string;
 }
 
 export interface IInputableAttributes extends ICommonAttributes, IInputableCommonAttributes {
-  name?: string;
-  alt?: string;
-  autocomplete?: IOnOff;
-  autofocus?: boolean;
-  checked?: boolean;
-  dirname?: string;
-  disabled?: boolean;
-  list?: string;
-  max?: number | string;
-  min?: number | string;
-  maxlength?: number;
-  minlength?: number;
-  multiple?: boolean;
-  pattern?: RegExp;
-  placeholder?: string;
-  readonly?: boolean;
-  required?: boolean;
-  size?: number;
-  // src?: string; // Input image type
-  step?: number;
-  value?: string;
+    name?: string;
+    alt?: string;
+    autocomplete?: IOnOff;
+    autofocus?: boolean;
+    checked?: boolean;
+    dirname?: string;
+    disabled?: boolean;
+    list?: string;
+    max?: number | string;
+    min?: number | string;
+    maxlength?: number;
+    minlength?: number;
+    multiple?: boolean;
+    pattern?: RegExp;
+    placeholder?: string;
+    readonly?: boolean;
+    required?: boolean;
+    size?: number;
+    // src?: string; // Input image type
+    step?: number;
+    value?: string;
 }
 
 export interface IFormAttributes extends ICommonAttributes {
-  acceptCharset?: string;
-  action?: string;
-  autocomplete?: 'on' | 'off';
-  enctype?: string;
-  method?: 'get' | 'post';
-  name?: string;
-  novalidate?: boolean;
-  rel?: IFormRelationship;
-  target?: ITarget;
+    acceptCharset?: string;
+    action?: string;
+    autocomplete?: 'on' | 'off';
+    enctype?: string;
+    method?: 'get' | 'post';
+    name?: string;
+    novalidate?: boolean;
+    rel?: IFormRelationship;
+    target?: ITarget;
 }
 
 export interface IVideoAttributes extends ICommonAttributes {
-  autoplay?: boolean;
-  controls?: boolean;
-  loop?: boolean;
-  muted?: boolean;
-  width?: string;
-  height?: string;
-  poster?: string;
-  preload?: 'auto' | 'metadata' | 'none';
-  src?: string;
+    autoplay?: boolean;
+    controls?: boolean;
+    loop?: boolean;
+    muted?: boolean;
+    width?: string;
+    height?: string;
+    poster?: string;
+    preload?: 'auto' | 'metadata' | 'none';
+    src?: string;
 }
 
 export interface ISourceProps extends ICommonAttributes {
-  srcset?: string;
-  media?: string;
-  src?: string;
-  sizes?: string;
-  type?: string;
+    srcset?: string;
+    media?: string;
+    src?: string;
+    sizes?: string;
+    type?: string;
 }
 
 export interface IProgressAttributes extends ICommonAttributes {
-  value?: string;
-  min?: string;
-  max?: string;
+    value?: string;
+    min?: string;
+    max?: string;
 }
 
 export interface IPictureAttributes extends ICommonAttributes {
-  source?: ISourceProps | ISourceProps[];
-  src?: string;
-  alt?: string;
+    source?: ISourceProps | ISourceProps[];
+    src?: string;
+    alt?: string;
 }
 
 export interface IImageAttributes extends ICommonAttributes {
-  src?: string;
-  alt?: string;
-  srcset?: string;
-  children?: undefined;
+    src?: string;
+    alt?: string;
+    srcset?: string;
+    children?: undefined;
 }
 
 export interface IAudioAttributes extends ICommonAttributes {
-  autoplay?: boolean;
-  controls?: boolean;
-  loop?: boolean;
-  muted?: boolean;
-  preload?: boolean;
-  src: string;
+    autoplay?: boolean;
+    controls?: boolean;
+    loop?: boolean;
+    muted?: boolean;
+    preload?: boolean;
+    src: string;
 }
 
 export interface IIFrameAttributes extends ICommonAttributes {
-  allow?: string;
-  allowfullscreen?: IBoolean;
-  allowpaymentrequest?: IBoolean;
-  loading?: ILoading;
-  name?: string;
-  referrerpolicy?: IReferrerPolicy;
-  sandbox?: ISandbox;
-  src: string;
+    allow?: string;
+    allowfullscreen?: IBoolean;
+    allowpaymentrequest?: IBoolean;
+    loading?: ILoading;
+    name?: string;
+    referrerpolicy?: IReferrerPolicy;
+    sandbox?: ISandbox;
+    src: string;
 }
 
 
@@ -326,9 +394,9 @@ export interface ITableHeadAttributes extends ITableCellAttributes {
 }
 
 export interface ITableCellAttributes extends ICommonAttributes {
-  scope?: string;
-  rowspan?: number;
-  colspan?: number;
+    scope?: string;
+    rowspan?: number;
+    colspan?: number;
 }
 
 
