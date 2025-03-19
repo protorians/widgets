7f5d914170ec01ec8aa1d670f79e71202dd35814
Protorians <protorians.dev@gmail.com>
2025-03-19 13:23:36 +0000

    Rename `declarationExcavates` to  `declarationExcavates`

--- source/helpers/declaration.ts
+++ source/helpers/declaration.ts
@@ -1,21 +1,41 @@
-import type {IAttributes, IWidgetDeclaration} from "../types/index.js";
+import type {IWidgetDeclaration, IWidgetDeclarationExploded} from "../types/index.js";
 
-export function declarationExcavates<E extends HTMLElement, A extends IAttributes, T>(
-  props: IWidgetDeclaration<E, A & T>,
-  targetKeys: (keyof IWidgetDeclaration<E, A & T>)[]
-) {
+export function declarationExplodes<D extends IWidgetDeclaration<any, any>, T>(
+    props: D,
+    targetKeys: (keyof T)[]
+): IWidgetDeclarationExploded<D, T> {
+    const extended: T = {} as T
+    const declaration: D = {} as D
 
-  const excavate: T = {} as T
-  const current: IWidgetDeclaration<E, A> = {} as IWidgetDeclaration<E, A>
+    Object.entries(props as Object)
+        .forEach(([key, value]) => {
+            if (targetKeys.includes(key as any)) {
+                extended[key] = value
+            } else {
+                declaration[key] = value;
+            }
+        })
 
-  Object.entries(props)
-    .forEach(([key, value]) => {
-      if (targetKeys.includes(key as keyof IWidgetDeclaration<E, A>)) {
-        excavate[key] = value
-      } else {
-        current[key] = value;
-      }
-    })
-
-  return {excavate, current}
+    return {extended, declaration}
 }
+
+
+// export function declarationExcavates<E extends HTMLElement, A extends IAttributes, T>(
+//   props: IWidgetDeclaration<E, A & T>,
+//   targetKeys: (keyof IWidgetDeclaration<E, A & T>)[]
+// ) {
+//
+//   const excavate: T = {} as T
+//   const current: IWidgetDeclaration<E, A> = {} as IWidgetDeclaration<E, A>
+//
+//   Object.entries(props)
+//     .forEach(([key, value]) => {
+//       if (targetKeys.includes(key as keyof IWidgetDeclaration<E, A>)) {
+//         excavate[key] = value
+//       } else {
+//         current[key] = value;
+//       }
+//     })
+//
+//   return {excavate, current}
+// }
