f7d548e29a5bd63c0f760d9435a6190993a90e8b
Protorians <129060085+protorians@users.noreply.github.com>
2025-05-27 13:46:57 +0000

    Add useStateless hook for handling stateless widget composite logic
    This new hook simplifies the management of stateless widgets by extracting values from dependencies and providing them to a composite function. It ensures seamless integration with StateWidget instances while maintaining flexibility for other dependency types.

---
+++ source/hooks/stateless.ts
@@ -0,0 +1,15 @@
+import {IAttributes, IComposite, IWidgetNode} from "../types/index.js";
+import {StateWidget} from "./state.js";
+
+export function useStateless<Props extends Record<string, any>, E extends HTMLElement, A extends IAttributes>(
+    composite: IComposite<Props>,
+    dependencies: Props
+): IWidgetNode<E, A> {
+    const props: Props = {} as Props;
+
+    Object.entries(dependencies).forEach(([key, value]) =>
+        props[key as keyof Props] = (value instanceof StateWidget ? value.value : value) as Props[keyof Props]
+    )
+    return composite(props);
+}
+
