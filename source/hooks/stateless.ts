30b41a0fe0f82fdadb10a09dad1096ed920405e9
Protorians <129060085+protorians@users.noreply.github.com>
2025-05-28 14:17:10 +0000

    Refactor `useStateless` to enforce `IStatelessProps` typing
    Updated `useStateless` to use `IStatelessProps` for better type safety and clarity. This ensures the composite function explicitly works with the expected stateless properties. Minor import adjustments were made to include the new type definition.

--- source/hooks/stateless.ts
+++ source/hooks/stateless.ts
@@ -1,8 +1,9 @@
-import {IAttributes, IComposite, IWidgetNode} from "../types/index.js";
+import {IAttributes, IComposite, IStatelessProps, IWidgetNode} from "../types/index.js";
 import {StateWidget} from "./state.js";
 
+
 export function useStateless<Props extends Record<string, any>, E extends HTMLElement, A extends IAttributes>(
-    composite: IComposite<Props>,
+    composite: IComposite<IStatelessProps<Props>>,
     dependencies: Props
 ): IWidgetNode<E, A> {
     const props: Props = {} as Props;
@@ -10,6 +11,5 @@ export function useStateless<Props extends Record<string, any>, E extends HTMLEl
     Object.entries(dependencies).forEach(([key, value]) =>
         props[key as keyof Props] = (value instanceof StateWidget ? value.value : value) as Props[keyof Props]
     )
-    return composite(props);
+    return composite(props as IStatelessProps<Props>);
 }
-
